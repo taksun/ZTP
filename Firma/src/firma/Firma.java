1d4117af88019bb380f616d89bc9919edad9f352
Karolina <k4rolajna@gmail.com>
2012-12-27 14:21:40 +0100

    aa

--- Firma/src/firma/Firma.java
+++ Firma/src/firma/Firma.java
@@ -14,6 +14,6 @@ public class Firma {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a= 0;
     }
 }
