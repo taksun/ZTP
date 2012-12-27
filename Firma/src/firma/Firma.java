0805c5c3abaffd533d04dfd3510d5933feaffe26
Jacek <astanoskillos@gmail.com>
2012-12-27 14:26:05 +0100

    aa

--- Firma/src/firma/Firma.java
+++ Firma/src/firma/Firma.java
@@ -14,6 +14,6 @@ public class Firma {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a= 0;
+        //pozdro dla Karoliny xd Xp x;PPpxpx;axpa
     }
 }
