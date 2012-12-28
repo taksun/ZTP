745b2781c4f868127ced75887fd0d27ac617ce45
Karolina <k4rolajna@gmail.com>
2012-12-28 13:37:25 +0100

    zamowienie
    do poprawy

--- Firma/src/firma/Zamowienie.java
+++ Firma/src/firma/Zamowienie.java
@@ -4,10 +4,23 @@
  */
 package firma;
 
+import java.util.Date;
+
 /**
  *
  * @author Karolina
  */
 public class Zamowienie {
+
+    
+    private int zamID;
+    private Date data;
+    private String status;// nie wiem czy tak, bo w sumie chyba mozna inta i zrobic 0-niezrealizowane, 1-czeka,2-zrealizowane.. dokładnie nie wiem
+
+    public Zamowienie(int zID, String d, String s) {
+        zamID = zID;
+        data = new Date();
+        status = s;
+    }
     
 }
