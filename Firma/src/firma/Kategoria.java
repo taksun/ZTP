c4cfed124fcc9ef2d84949b7e2ab4136ec2cee2c
Karolina <k4rolajna@gmail.com>
2012-12-28 13:07:05 +0100

    klasa kategoria
    do uzupełnienia( prosze o info)

---
+++ Firma/src/firma/Kategoria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firma;
+
+/**
+ *
+ * @author Karolina
+ */
+public class Kategoria {
+    
+    private int produktID;
+    private String nazwa;
+    
+    public Kategoria(int kID,String n) {
+        produktID = kID;
+        nazwa = n;
+    }
+    //nie wiem jakie powinna ta klassa miec funkcje.. wyświetlanie wszystkich kategorii? czy co?
+}
